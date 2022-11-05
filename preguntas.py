102f0c6c4f88658ed4da5e14bd913865775ea38e
NataliaPalacio <npalacioq@unal.edu.co>
2022-11-05 13:33:56 -0500

    RL SIMPLE

--- preguntas.py
+++ preguntas.py
@@ -18,8 +18,8 @@ def pregunta_01():
     df = pd.read_csv('gm_2008_region.csv', encoding='latin-1',sep=',')
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
-    y = df['life']
-    X = df['fertility']
+    y = df['life'].values
+    X = df['fertility'].values
 
     # Imprima las dimensiones de `y`
     print(y.shape)
@@ -28,10 +28,10 @@ def pregunta_01():
     print(X.shape)
 
     # Transforme `y` a un array de numpy usando reshape
-    y_reshaped = y.values.reshape(-1, 1)
+    y_reshaped = y.reshape(-1, 1)
 
     # Trasforme `X` a un array de numpy usando reshape
-    X_reshaped = X.values.reshape(-1, 1)
+    X_reshaped = X.reshape(-1, 1)
 
     # Imprima las nuevas dimensiones de `y`
     print(y_reshaped.shape)
@@ -53,16 +53,16 @@ def pregunta_02():
     print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(round(df['life'].corr(df['fertility'], method='pearson'),4))
+    print(df['life'].corr(df['fertility']).round(4))
 
     # Imprima la media de la columna `life` con 4 decimales.
     print(round(df['life'].mean(),4))
 
     # Imprima el tipo de dato de la columna `fertility`.
-    print(df['fertility'].__class__)
+    print(type(df['fertility']))
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    print(round(df['life'].corr(df['GDP'], method='pearson'),4))
+    print(df['GDP'].corr(df['life']).round(4))
 
 
 def pregunta_03():
@@ -75,10 +75,12 @@ def pregunta_03():
     df = pd.read_csv('gm_2008_region.csv', sep=',')
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = np.array(df['fertility'])
+    X_fertility = df['fertility'].values
+    X_fertility = X_fertility.reshape(-1, 1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = np.array(df['life'])
+    y_life = df['life'].values
+    y_life = y_life.reshape(-1,1)
 
     # Importe LinearRegression
     from sklearn.linear_model import LinearRegression
@@ -94,14 +96,14 @@ def pregunta_03():
     ).reshape(-1, 1)
 
     # Entrene el modelo usando X_fertility y y_life
-    reg.fit(X_fertility.reshape(-1,1), y_life.reshape(-1,1))
+    reg.fit(X_fertility, y_life)
 
     # Compute las predicciones para el espacio de predicción
     y_pred = reg.predict(prediction_space)
 
     # Imprima el R^2 del modelo con 4 decimales
     #print(____.score(____, ____).round(____))
-    print("{:.4f}".format(reg.score(X_fertility.reshape(-1,1), y_life.reshape(-1,1))))
+    print(round(reg.score(X_fertility,y_life),4))
 
 
 def pregunta_04():
@@ -121,10 +123,12 @@ def pregunta_04():
     df = pd.read_csv('gm_2008_region.csv', sep=',')
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = np.array(df['fertility'])
+    X_fertility = df['fertility'].values
+    X_fertility = X_fertility.reshape(-1,1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = np.array(df['life'])
+    y_life = df['life'].values
+    y_life = y_life.reshape(-1,1)
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
@@ -139,10 +143,10 @@ def pregunta_04():
     linearRegression = LinearRegression()
 
     # Entrene el clasificador usando X_train y y_train
-    linearRegression.fit(X_train.reshape(-1,1), y_train.reshape(-1,1))
+    linearRegression.fit(X_train, y_train)
 
     # Pronostique y_test usando X_test
-    y_pred = linearRegression.predict(X_test.reshape(-1,1))
+    y_pred = linearRegression.predict(X_test)
 
     # Compute and print R^2 and RMSE
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
